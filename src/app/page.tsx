d438700f1b72dbb7be0c758789fec9e262b6c050
yu.wang <wangyu@wycode.cn>
2026-03-28 02:23:44 +0800

    fix: add back add view component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { getDataSource } from "@/app/actions/dataSource";
 import { useDataSourceStore } from "@/store/useDataSourceStore";
 import { DataSourceManager } from "@/components/cards/datasource/DataSourceManager";
 import { ViewGrid } from "@/components/cards/view/ViewGrid";
+import { AddViewCard } from "@/components/cards/view/AddViewCard";
 import { AIConfigAlert } from "@/components/cards/datasource/AIConfigAlert";
 
 export default function Home() {
@@ -28,6 +29,7 @@ export default function Home() {
     <main className="container mx-auto p-4 md:p-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 md:gap-6 auto-rows-auto">
       {!aiConfigured && <AIConfigAlert />}
       <DataSourceManager />
+      {currentDataSource && <AddViewCard aiConfigured={aiConfigured} />}
       {currentDataSource && <ViewGrid />}
     </main>
   );
